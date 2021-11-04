437518770082db929b80f6e641401a4190ee8f55
zeldon-0 <48602270+zeldon-0@users.noreply.github.com>
2021-11-05 01:06:57 +0200

    Add the comments

--- Lab-4/fp-4-1.rkt
+++ Lab-4/fp-4-1.rkt
@@ -3,53 +3,72 @@
 ; ІПЗ-42
 ; Л.р. 4, завдання 14.1
 
+; процедура обчислення значення n-ого члена послідовності
 (define (hexagonal-number n)
   (-(* 2 n n) n)
 )
 
+; процедура створення списку 6-кутних чисел
 (define (make-list size)
   (make-list-inner '() size)
 )
 
+; процедура для рекурсивного створення списку 6-кутних чисел по елементу за виклик
 (define (make-list-inner current-list size)
+  ; якщо поточний - початковий - список порожій, проініціювати його першим елементом послідовності
   (cond [(null? current-list)
          (make-list-inner (list (hexagonal-number 1)) size)]
+        ; якщо поточний список менший за заданий розмір, доповнити його наступним елементом послідовності
         [(< (length current-list) size)
          (make-list-inner (append current-list (list (hexagonal-number (+ 1 (length current-list))))) size)]
+        ; якщо поточний список - заданого розміру, повернути його
         [(= (length current-list) size)
          current-list]
  )
 )
 
+; процедура для обчислення суми парних елементів списку
 (define (even-member-sum list)
   (even-member-sum-inner list 0)
 )
 
+; внутрішня процедура для обчислення суми парнихз елементів у поточному підсписку
 (define (even-member-sum-inner list current-sum)
+  ; якщо поточний - кінцевий - список порожій, повернути поточну суму
   (cond [(null? list)
          current-sum]
+        ; якщо перший елемент поточного списку - парний, викликати процедуру для решти списку та поточної суми + перший елемент
         [(even? (car list))
          (even-member-sum-inner (cdr list) (+ (car list) current-sum))]
+        ; якщо перший елемент поточного списку - не парний, викликати процедуру для решти списку та поточної суми
         [(odd? (car list))
          (even-member-sum-inner (cdr list) current-sum)]
   ) 
 )
 
+; процедура для заміни елементів згідно з умовою
 (define (replace-elements current-list)
+  ; якщо поточний - кінцевий - список порожій, повернути поточний список
   (cond [(null? current-list)
          current-list]
+        ; якщо поточний список починається з числа, кратного 5, повернути список, утворений з початкового елементу, помноженого на 10, та списку, отриманого викликом процедури для решти списку 
         [(= (modulo (car current-list) 5) 0)
          (append (list (* 10 (car current-list))) (replace-elements (cdr current-list)))]
+        ; інакше повернути список, утворений з початкового елементу та списку, отриманого викликом процедури для решти списку        
         [else
          (append (list (car current-list)) (replace-elements (cdr current-list)))]
   ) 
 )
 
+; процедура для формування підсписку згідно з умовою
 (define (get-subset current-list)
+  ; якщо поточний - кінцевий - список порожій, повернути поточний список
   (cond [(null? current-list)
          current-list]
+        ; якщо поточний список починається з числа, кратного 10, повернути список, утворений з початкового елементу та списку, отриманого викликом процедури для решти списку 
         [(= (modulo (car current-list) 10) 0)
          (append (list (car current-list)) (get-subset (cdr current-list)))]
+        ; інакше повернути список, отриманоий викликом процедури для решти списку         
         [else
          (get-subset (cdr current-list))]
   ) 
--- Lab-4/fp-4-2.rkt
+++ Lab-4/fp-4-2.rkt
@@ -3,21 +3,34 @@
 ; ІПЗ-42
 ; Л.р. 4, завдання 14.2
 
+; кількість товару в кожній точці
 (define store-stock (list 50 70 100 30 40 90))
+; тривалість очікування кожною точкою наступної поставки товару
 (define store-restock-wait (list 0 0 0 0 0 0))
+; статус відправки товару в кожну точку
 (define store-restock-wait-status (list #f #f #f #f #f #f))
+; кількість товару, що поставляєтсья в кожну точку
 (define store-restock-size 65)
+; кількість товару, що продається точкою за день
 (define store-stock-loss 10)
+; тривалість очікуання на поставку товару в точку
 (define store-restock-delay 6)
+; кількість товару в оптовому магазині
 (define warehouse-stock 300)
+; кількість товару, що поставляється в оптовий магазин за раз
 (define warehouse-restock-size 1000)
+; тривалість поставки товару в оптовий магазин
 (define warehouse-restock-delay 90)
+; тривалість часу між замовленнями поставок товару в оптовий магазин
 (define warehouse-restock-frequency 14)
+; черга з тривалості часу до поставки товару в оптовий магазин
 (define warehouse-restock-queue '())
+; дефіцит товару в оптових точках за день
 (define deficit 0)
+; накопичений дефіцит товару в оптових точках за тривалість симуляції
 (define overall-deficit 0)
 
-
+; процедура для отримання значення у списку за індексом
 (define (get-list-element elements n) 
   (if (= n 1) 
       (car elements)
@@ -25,6 +38,7 @@
   )
 )
 
+; процедура для встановлення значення у списку за індексом
 (define (set-list-element elements n value) 
   (if (= n 1) 
       (append (list value) (cdr elements))
@@ -32,11 +46,14 @@
   )
 )
 
+; основна процедура для симуляції роздрібної торгівлі
 (define (simulate days)
   (simulate-internal 1 days)
 )
 
+; внутрішня процедура для симуляції роздрібної торгівлі за заданий день
 (define (simulate-internal current-day days)
+  ; поки не настав кінецвий день, викликаються процедури для симуляції окремих процесів торгівлі
   (cond [(< current-day days)
            (begin
              (sell-stock)
@@ -48,6 +65,7 @@
              (simulate-internal (+ 1 current-day) days)
             )
          ]
+        ; якщо настав кінецвий день, викликається процедура обчислення незадоволеного попиту за останній день та виведення інформації про незадоволений попит в цілому 
         [else
          (begin
            (calculate-deficit 1)
@@ -55,17 +73,20 @@
          )]
   )
 )
-
+; процедура симуляції продажу товару за день
 (define (sell-stock)
   (sell-stock-internal 1)
 )
 
+; внутрішня процедура симуляції продажу однією точкою товару за день
 (define (sell-stock-internal n)
+  ; товар продається доти, доки поточний індекс належить списку, і поточна точка має товар
   (cond[(and (< n 7) (or (> (get-list-element store-stock n)  store-stock-loss) (= (get-list-element store-stock n)  store-stock-loss)))
         (begin
         (set! store-stock (set-list-element store-stock n (- (get-list-element store-stock n) store-stock-loss)))
         (sell-stock-internal (+ n 1))
         )]
+       ; якщо поточний індекс належить списку, але поточна точка має дефіцит товару, незадоволений попит додається до поточного показника незадоволеного попиту
        [(and (< n 7) (< (get-list-element store-stock n)  store-stock-loss))
         (begin
         (set! overall-deficit (+ overall-deficit (- store-stock-loss (get-list-element store-stock n))))
@@ -75,11 +96,14 @@
    )
 )
 
+; процедура симуляції поставки товару в точки
 (define (send-stock)
   (send-stock-internal 1)
 )
 
+; внутрішня процедура симуляції поставки товару в поточну точку
 (define (send-stock-internal n)
+  ; якщо поточна точка не очікує на поставку та час очікування становить 0, встановити показник очікування в істине значення, а тривалість очікування - у задане значення
   (cond[(and (< n 7) (or (> warehouse-stock store-restock-size) (= warehouse-stock store-restock-size)) (= (get-list-element  store-restock-wait n) 0)
              (not(get-list-element  store-restock-wait-status n)))
         (begin
@@ -91,16 +115,20 @@
    )
 )
 
+; процедура симуляції очікування поставки товару в точки
 (define (await-stock)
   (await-stock-internal 1)
 )
 
+; внутрішня процедура симуляції очікування поставки товару в задану точку
 (define (await-stock-internal n)
+  ; якщо тривалість очікування на товар не є нульовою, зменшити тривалість на 1
   (cond[(and (< n 7) (> (get-list-element  store-restock-wait n) 0))
         (begin
         (set! store-restock-wait (set-list-element store-restock-wait n (- (get-list-element  store-restock-wait n) 1)))
         (await-stock-internal (+ n 1))
         )]
+      ; якщо тривалість очікування на товар є нульовою, і показник очікування поставлено в істине значення, додати до кількості товару на точці задану кількість, а показник зробити хибним 
        [(and (< n 7) (= (get-list-element  store-restock-wait n) 0) (get-list-element  store-restock-wait-status n))
         (begin
         (set! store-stock (set-list-element store-stock n (+ (get-list-element  store-stock n) store-restock-size)))
@@ -110,26 +138,34 @@
    )
 )
 
+; процедура симуляції відправлення товару в оптовий магазин
 (define (send-warehouse-restock-order current-day)
+  ; з заданою періодичністю викликається внутрішня процедура
   (cond[(= (modulo current-day warehouse-restock-frequency) 0)
         (send-warehouse-restock-order-internal)]
   )
 )
 
+; внутрішня процедура симуляції відправлення товару в оптовий магазин
 (define (send-warehouse-restock-order-internal)
+  ; до списку очікуваних поставок додається нова - з заданою тривалістю
   (set! warehouse-restock-queue (append warehouse-restock-queue (list warehouse-restock-delay)))
 )
 
+; процедура симуляції очікування оптовим магазином на поставку товару
 (define (await-warehouse-restock)
   (await-warehouse-restock-internal 1)
 )
 
+; внутрішня процедура симуляції очікування оптовим магазином на поставку товару
 (define (await-warehouse-restock-internal n)
+  ; якщо очікування на поточну поставку не скінчилось, зменшити його тривалість на 1
   (cond[(and (< n (+ (length warehouse-restock-queue) 1)) (> (get-list-element  warehouse-restock-queue n) 0))
         (begin
         (set! warehouse-restock-queue (set-list-element warehouse-restock-queue n (- (get-list-element  warehouse-restock-queue n) 1)))
         (await-warehouse-restock-internal  (+ n 1))
         )]
+       ; якщо очікування на поточну поставку скінчилось, прибрати перший елемент списку очікування, а кількість товару в оптовому магазині збільшити
        [(and (= n 1) (not (null? warehouse-restock-queue)) (= (get-list-element warehouse-restock-queue n) 0))
         (begin
         (set! warehouse-stock (+ warehouse-stock warehouse-restock-size))
@@ -139,6 +175,7 @@
    )
 )
 
+; процедура обчислення кількості незадоволеного попиту між точками за день 
 (define (calculate-deficit n)
     (cond[(and (< n 7) (< (get-list-element  store-stock n) store-stock-loss))
           (begin 
@@ -149,7 +186,8 @@
           (calculate-deficit (+ n 1))]
     )
 )
-  
+
+; процедура друку інформації про незадоволений попит  
 (define (print-deficit)
   (begin
     (display "The amout of demand that will not be satisfied the next day: ")
@@ -161,7 +199,7 @@
   )
 )
 
-  
+; процедура друку інформації про результати продажів за поточний день 
 (define (print-status current-day)
   (begin
     (display "Day ")
@@ -186,7 +224,3 @@
 (display "\n")
 (simulate 150)
 (display "\n")
-
-
-
-
